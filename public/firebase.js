28c52d2d2dce5f4d4728383a4fa965cc10d85230
eric323c <71191021+eric323c@users.noreply.github.com>
2024-12-02 02:46:55 -0500

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,32 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
-
+// Firebase Configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyDK7QoOBnf73q8V0H7vdZgeV9knY1GQhZw",
+  authDomain: "mandi-b5b19.firebaseapp.com",
+  projectId: "mandi-b5b19",
+  storageBucket: "mandi-b5b19.appspot.com",
+  messagingSenderId: "53505043322",
+  appId: "1:53505043322:web:dede68f8aeca80c7de84c3",
+  measurementId: "G-T58TB5HC3L"
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export const getGuestByName = async (firstName, lastName) => {
-  const docRef = doc(db, "guests", `${firstName}_${lastName}`);
-  const docSnap = await getDoc(docRef);
-  return docSnap.exists() ? docSnap.data() : null;
-};
-
-export const createGuest = async (firstName, lastName, role) => {
-  const docRef = doc(db, "guests", `${firstName}_${lastName}`);
-  const defaultData = {
-    firstName,
-    lastName,
-    role,
-    rsvpStatus: "Pending",
-  };
-  await setDoc(docRef, defaultData);
-  return defaultData;
-};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
